05197f1ad39be99f6a637ca0e93916fcac8223a4
ibrahimnagib <ibrahim.nagib@utexas.edu>
2015-10-14 19:11:56 -0500

    commiting first plot, ETL files, and data wrangling

--- 01 Data/Project3.R
+++ 01 Data/Project3.R
@@ -2,8 +2,15 @@ require("jsonlite")
 require("RCurl")
 
 # Change the USER and PASS below to be your UTEid
-df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from undata"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_in2422', PASS='orcl_in2422', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+YL <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from youthlit"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_in2422', PASS='orcl_in2422', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
 
-str(df)
-summary(df)
-head(df)
+str(YL)
+summary(YL)
+head(YL)
+
+PG <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from percentGDP"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_in2422', PASS='orcl_in2422', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+
+str(PG)
+summary(PG)
+head(PG)
+
---
+++ 02 Data Wrangling/Plot3.R
@@ -0,0 +1,33 @@
+require(tidyr)
+require(dplyr)
+require(ggplot2)
+require(plyr)
+
+# Move data into new data frame for Plot 1
+
+Plot1_df <- dplyr::inner_join(YL, PG, by= c("TIME_PERIOD", "REFERENCE_AREA")) 
+
+# # Need to relabel the data in the Education column
+# Plot1_df$RESTAURANT <- factor(Plot1_df$RESTAURANT, levels=c("m", "b", "p", "t","w", "j", "h", "c", "i", "k"), labels = c("McDonald's", "Burger King", "Pizza Hut", "Taco Bell", "Wendy's", "Jack In The Box", "Hardee's", "Carl's Jr.", "In-N-Out", "KFC")) 
+
+Plot1_df <- Plot1_df %>% filter(TIME_PERIOD  <= 1995, SEX.x == "All genders" )
+# 
+# test_df <- ddply(Plot1_df,~ZIP, summarise,Total_Restaurants=length(RESTAURANT))
+# Plot1_df <- dplyr::left_join(Plot1_df,test_df, by="ZIP")
+# Plot1_df <- Plot1_df %>% select(ZIP, MEDIAN, Total_Restaurants)
+
+ggplot() +
+  coord_cartesian() + 
+  scale_x_continuous() +
+  scale_y_continuous() +
+  #facet_wrap(~RESTAURANT) +
+  labs(title="PLOT 1") +
+  labs(x="X", y="Y", color="REFERENCE_AREA") +
+  layer(data=Plot1_df , 
+        mapping=aes(x=as.numeric(OBSERVATION_VALUE.y), y=as.numeric(OBSERVATION_VALUE.x), color = as.character(REFERENCE_AREA)), 
+        stat="identity",
+        stat_params=list(), 
+        geom="point",
+        geom_params=list(), 
+        position=position_jitter(width=0.0, height=0)
+  )
